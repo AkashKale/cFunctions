82cb3568248e71f2e117b5c9d50861172d947ffb
Prathamesh Apte <prathameshapte105@gmail.com>
2015-08-28 00:05:40 +0530

    Splash Screen Activity

---
+++ app/src/main/java/com/conrover/cfunctions/Splash.java
@@ -0,0 +1,38 @@
+package com.conrover.cfunctions;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by sony on 27/08/2015.
+ */
+public class Splash extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread timer=new Thread(){
+            public void run()
+            {
+                try{
+                    sleep(5000);
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally{
+                    startActivity(new Intent("android.intent.action.MainActivity"));
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        finish();
+        super.onPause();
+    }
+}
