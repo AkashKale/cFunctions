93860c9abc70a631e285c0d10b5a8c91776dbc7f
Prathamesh Apte <prathameshapte105@gmail.com>
2015-09-09 22:32:19 +0530

    See Also feature Implemented

--- app/src/main/java/com/conrover/cfunctions/DetailsActivity.java
+++ app/src/main/java/com/conrover/cfunctions/DetailsActivity.java
@@ -2,12 +2,19 @@ package com.conrover.cfunctions;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,32 +26,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by sony on 29/08/2015.
  */
-public class DetailsActivity extends ActionBarActivity {
+public class DetailsActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
     TextView tvHeaderFile,tvSyntax,tvReturns,tvParameters,tvDesc;
     String function_name,header,synt,desc,par,ret,hea;
+    ListView lvSee;
+    ArrayList<String> SimilarFunList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailsactivity);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        //TextView tv= (TextView) findViewById(R.id.tvvvv);
         Bundle bundle=getIntent().getExtras();
         header= bundle.getString("header");
         function_name=bundle.getString("function_name");
-        //tv.setText("Header="+header+" func_name="+function_name);
         tvHeaderFile= (TextView) findViewById(R.id.tvHeaderFile);
         tvSyntax= (TextView) findViewById(R.id.tvSyntax);
         tvReturns= (TextView) findViewById(R.id.tvReturns);
         tvParameters= (TextView) findViewById(R.id.tvParameters);
         tvDesc= (TextView) findViewById(R.id.tvDesc);
+        lvSee= (ListView) findViewById(R.id.lvSee);
+        lvSee.setOnItemClickListener(this);
         new loadfunction().execute();
 
+    }
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        //Log.e("sdasfafdaffa","yeessssssssssss");
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -72,6 +103,14 @@ public class DetailsActivity extends ActionBarActivity {
         }
         return null;
     }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String funname=(String)lvSee.getItemAtPosition(position);
+        function_name=(String)lvSee.getItemAtPosition(position);
+        Log.e("onitemclick",funname);
+        new loadfunction().execute();
+    }
+
     public class loadfunction extends AsyncTask<Void,Integer,String>{
 
         @Override
@@ -90,25 +129,24 @@ public class DetailsActivity extends ActionBarActivity {
                         ret=jo_inside.getString("returns");
                         par=jo_inside.getString("parameters");
                         desc=jo_inside.getString("desc");
-                       // Log.e("syntax---",jo_inside.getString("returns"));
-                        //tvHeaderFile.setText(header);
-                        //tvSyntax.setText(synt);
-                        //tvReturns.setText(ret);
-                        //tvParameters.setText(par);
-                        //tvDesc.setText(desc);
-                        //break;
+                        JSONArray seealso=jo_inside.getJSONArray("seealso");
+                        SimilarFunList=new ArrayList<String>();
+                        for(int j=0;j<seealso.length();j++)
+                        {
+                            JSONObject similar=seealso.getJSONObject(j);
+                            SimilarFunList.add(similar.getString("similarfun"));
+                        }
+
                     }
-                    //temp.add(name);
-                    //formList.add(m_li);
+
                 }
                 return ret;
             } catch (JSONException e) {
                 e.printStackTrace();
-                //tvHeaderFile.setText("heeeee");
-                //Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+
             }
             return ret;
-            //return null;
+
         }
 
        @Override
@@ -120,6 +158,26 @@ public class DetailsActivity extends ActionBarActivity {
             tvReturns.setText(ret);
             tvParameters.setText(par);
             tvDesc.setText(desc);
+           ArrayAdapter<String> adapter = new ArrayAdapter<String>(DetailsActivity.this, android.R.layout.simple_list_item_1,SimilarFunList) {
+
+               @Override
+               public View getView(int position, View convertView,
+                                   ViewGroup parent) {
+                   View view =super.getView(position, convertView, parent);
+
+                   TextView textView=(TextView) view.findViewById(android.R.id.text1);
+
+            /*YOUR CHOICE OF COLOR*/
+                   textView.setTextColor(Color.WHITE);
+
+                   return view;
+               }
+           };
+           lvSee.setAdapter(adapter);
+           //lvSee.setAdapter(new ArrayAdapter<String>(DetailsActivity.this, android.R.layout.simple_list_item_1, SimilarFunList));
+           setListViewHeightBasedOnChildren(lvSee);
+           lvSee.setOnItemClickListener(DetailsActivity.this);
+
         }
     }
 }
